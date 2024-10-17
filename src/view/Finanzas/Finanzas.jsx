40aab963e380d9fbac456ff72f25926f63dd9b9c
Camilo Urrea <luissepulveda110@gmail.com>
2024-10-17 18:07:35 -0500

    Update Finanzas.jsx
    Se terminan todas las funcionalidades

--- src/view/Finanzas/Finanzas.jsx
+++ src/view/Finanzas/Finanzas.jsx
@@ -1,7 +1,31 @@
 import { TransaccionItem } from "./Components/TransaccionItem"
 import '../../styles/Finanzas.css'
+import { useEffect } from "react"
+import { getAllTransaccion, getResumen } from "../../services/transaccion"
+import { useState } from "react"
+import { TransaccionForm } from "./Components/TransaccionForm"
 
 export const Finanzas = () => {
+
+  const [transacciones, setTransacciones] = useState([])
+  const [resumen, setResumen] = useState({Ingreso: 0, Egreso: 0, Balance: 0})
+
+  useEffect(() => {
+    fetchTransacciones()
+    fetchResumen()
+  }, [])
+
+  const fetchTransacciones = async () => {
+    const resp = await getAllTransaccion()
+    setTransacciones(resp)
+  }
+
+  const fetchResumen = async() => {
+    const resp = await getResumen()
+    setResumen(resp)
+  }
+
+
   return (
     <div className="finanzas">
       <h1 className="title"> Finanzas </h1>
@@ -15,104 +39,44 @@ export const Finanzas = () => {
                   <img src="src/assets/img/ingresos.png" alt="icon"/>
                   <div className="valor">
                   <h3>Ingresos</h3>
-                    <p>$ 1.000.000</p>
+                    <p>$ {resumen.Ingreso}</p>
                   </div>
                 </div>
                 <div className="egresos">
                 <img src="src/assets/img/egresos.png" alt="icon"/>
                   <div className="valor">
                     <h3>Egresos</h3>
-                    <p>$ 1.000.000</p>
+                    <p>$ {resumen.Egreso}</p>
                   </div>
                 </div>
-              </div>
-              <div className="parte-inferior">
                 <div className="total">
                 <img src="src/assets/img/balance.png" alt="icon"/>
                   <div className="valor">
                   <h3>Balance</h3>
-                    <p>$ 1.000.000</p>
+                    <p>$ {resumen.Total}</p>
                   </div>
-                </div>
+              </div>
               </div>
             </div>
           </div>
 
-          <div className="form">
-            <div className="form-headear">
-              <h2>Registrar Transacción </h2>
-            </div>
+          <TransaccionForm />
 
-            <div className="form-body">
-              <div className="form-tipo">
-                <a> Leche </a>
-                <a> Carne </a>
-                <a> General </a>
-              </div>
-
-              <div className="form-input">
-                <label> Descripción </label>
-                <input type="text" />
-              </div>
-
-              <div className="form-input">
-                <label> Código Producto </label>
-                <input type="text" />
-              </div>
-
-              <div className="form-input">
-                <label> Cantidad </label>
-                <input type="text" />
-              </div>
-
-              <div className="form-input">
-                <label> Fecha </label>
-                <input type="date"/>
-              </div>
-
-              <div className="form-input">
-                <label> Valor </label>
-                <input type="text"/>
-              </div>
-
-              <div className="form-input">
-                <label> Observaciones </label>
-                <textarea></textarea>
-              </div>
-
-              <div className="form-submit">
-                <button> Ingreso </button>
-                <button> Egreso </button>
-              </div>
-
-            </div>          
           </div>
-        </div>
+          
 
         <div className="lista-transacciones">
             <h2>Transacciones</h2>
             <input type="text" placeholder="Buscar Registro" />
             <div className="scroll-trasacciones">
-              <TransaccionItem
-                ID="1"
-                Fecha="2021-08-01"
-                Valor={10000}
-                Producto="Leche"
-              />
-              <TransaccionItem
-                ID="2"
-                Fecha="2021-09-01"
-                Valor={20000}
-                Producto="Carne"
-              />
-              <TransaccionItem
-                ID="3"
-                Fecha="2022-09-01"
-                Valor={15000}
-                Producto="Insumos"
-              />
+              {
+                transacciones.map((item) => (
+                  <TransaccionItem key={item.ID} item={item} />
+                ))
+              }
             </div>
           </div>
+          
 
       </div>
     </div>
