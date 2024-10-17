cb3e8e17180134b4aed68d5e7b077662b28a26d5
Camilo Urrea <luissepulveda110@gmail.com>
2024-10-17 16:49:38 -0500

    Update TransaccionItem.jsx
    Se organiza el icono variable para cada item de transaccion

--- src/view/Finanzas/Components/TransaccionItem.jsx
+++ src/view/Finanzas/Components/TransaccionItem.jsx
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types"
-export const TransaccionItem = ({ID, Fecha, Valor, Producto}) => {
+export const TransaccionItem = ({item: {ID, Fecha, Valor, Producto, Tipo}}) => {
   
   const handleClick = () => {
     console.log(ID)
@@ -7,7 +7,10 @@ export const TransaccionItem = ({ID, Fecha, Valor, Producto}) => {
 
   return (
     <div className="TransaccionItem" onClick={handleClick}>
-      <img src="src/assets/img/positivo.png" alt="icon"/>
+      <img 
+        src={(Tipo == "Ingreso") ? "src/assets/img/positivo.png" : "src/assets/img/negativo.png"}
+        alt="icon"
+      />
       <p> {Fecha} </p>
       <div className="producto-valor">
         <h3> {Producto} </h3>
@@ -19,10 +22,13 @@ export const TransaccionItem = ({ID, Fecha, Valor, Producto}) => {
 }
 
 TransaccionItem.propTypes = {
-  ID: PropTypes.string,
-  Fecha: PropTypes.string,
-  Valor: PropTypes.number,
-  Producto: PropTypes.string,
+  item : PropTypes.shape({
+    ID: PropTypes.string,
+    Fecha: PropTypes.string,
+    Valor: PropTypes.string,
+    Producto: PropTypes.string,
+    Tipo: PropTypes.string
+  })
 }
 
 
