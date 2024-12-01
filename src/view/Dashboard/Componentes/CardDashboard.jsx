97904fc46065cdbd7e81fdffea2b85497b34233b
GustavoA198 <adolfogutierreza0108@gmail.com>
2024-12-01 12:11:19 -0500

    ajuste info dash

--- src/view/Dashboard/Componentes/CardDashboard.jsx
+++ src/view/Dashboard/Componentes/CardDashboard.jsx
@@ -1,10 +1,13 @@
-import '../styles/Dash.css';
+import '../../../styles/Dash.css';
 import PropTypes from 'prop-types';
 
 export const CardDashboard = ({ title, info }) => {
   return (
     <div className="card-dashboard">
-      <h3>{title}</h3>
+      <div className="title">
+        <img src="https://img.icons8.com/ios/452/cow.png" alt="icon"/>
+        <h3>{title}</h3>
+      </div>      
       <h2>{info}</h2>
     </div>
   );
--- src/view/Dashboard/Dashboard.jsx
+++ src/view/Dashboard/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { CardDashboard } from "../../components/CardDashboard";
+import { CardDashboard } from "./Componentes/CardDashboard";
 import {ProductionChart} from  "./Componentes/ProduccionGrafico";
 import { GraficoTorta } from "./Componentes/GraficoTorta";
 import { InformeFinanzas } from "./Componentes/InformeFinanzas";
@@ -20,6 +20,103 @@ export function Dashboard() {
     setDateRange({...dateRange, [key]: value});
   }
 
+  /* data de prueba antes del fetch */
+  const DataHebrasMachos = {
+    labels: ['Hembras', 'Machos'],
+    datasets: [
+      {
+        label: 'Distribución de Animales',
+        data: [31, 11], // Datos de ejemplo
+        backgroundColor: ['rgba(75, 192, 192, 0.6)', 'rgba(255, 99, 132, 0.6)'],
+        borderColor: ['rgba(75, 192, 192, 1)', 'rgba(255, 99, 132, 1)'],
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const DataTiposAnimales = {
+    labels: ['Leche', 'Carne', 'Doble Propósito'],
+    datasets: [
+      {
+        label: 'Distribución por Tipo de Animales',
+        data: [25, 12, 5], // Datos de ejemplo
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)', 
+          'rgba(255, 99, 132, 0.6)', 
+          'rgba(54, 162, 235, 0.6)'
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(54, 162, 235, 1)'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+ 
+
+  const DataRazas = {
+    labels: ['Holstein', 'Jersey', 'Normando', 'Otros'],
+    datasets: [
+      {
+        label: 'Distribución de Razas',
+        data: [18, 10, 10, 4], // Datos de ejemplo
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)', 
+          'rgba(255, 99, 132, 0.6)', 
+          'rgba(54, 162, 235, 0.6)', 
+          'rgba(153, 102, 255, 0.6)'
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(54, 162, 235, 1)', 
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+  const DataEdadesAnimales = {
+    labels: [
+      '0 a 3 meses', 
+      '3 a 9 meses', 
+      '9 a 12 meses', 
+      '1 a 2 años', 
+      '2 a 3 años', 
+      '3 a 5 años', 
+      'Mayores de 5 años'
+    ],
+    datasets: [
+      {
+        label: 'Distribución por Edades',
+        data: [5, 8, 4, 10, 7, 5, 3], // Datos de ejemplo que suman 42
+        backgroundColor: [
+          'rgba(75, 192, 192, 0.6)', 
+          'rgba(255, 99, 132, 0.6)', 
+          'rgba(54, 162, 235, 0.6)', 
+          'rgba(153, 102, 255, 0.6)', 
+          'rgba(255, 206, 86, 0.6)', 
+          'rgba(201, 203, 207, 0.6)', 
+          'rgba(255, 159, 64, 0.6)'
+        ],
+        borderColor: [
+          'rgba(75, 192, 192, 1)', 
+          'rgba(255, 99, 132, 1)', 
+          'rgba(54, 162, 235, 1)', 
+          'rgba(153, 102, 255, 1)', 
+          'rgba(255, 206, 86, 1)', 
+          'rgba(201, 203, 207, 1)', 
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1,
+      },
+    ],
+  };
+  
+
   return (
     <div className="dashboardMain">
       <div className="header">
@@ -37,20 +134,20 @@ export function Dashboard() {
       </div>
       <div className="dashboard">
         <div className="cards">
-          <CardDashboard title="Total de animales" info="200" />
-          <CardDashboard title="Total de hembras" info="100" />
-          <CardDashboard title="Total de machos" info="100" />
-          <CardDashboard title="Total de montas" info="100" />
+          <CardDashboard title="Total de animales" info="42" />
+          <CardDashboard title="Total de partos" info="2" />
+          <CardDashboard title="Total de leche(lts)" info="641" />
+          <CardDashboard title="Total de carne(kg)" info="859" />
         </div>
         <div className="graficas">
           <ProductionChart/>
           <InformeFinanzas startDate={dateRange.start} endDate={dateRange.end}/>
         </div>
         <div className="graficos-torta">
-          <GraficoTorta/>
-          <GraficoTorta/>
-          <GraficoTorta/>
-          <GraficoTorta/>
+        <GraficoTorta data={DataHebrasMachos} title="Distribución de Animales" />
+        <GraficoTorta data={DataTiposAnimales} title="Distribución de Animales" />
+        <GraficoTorta data={DataRazas} title="Distribución de Animales" />
+        <GraficoTorta data={DataEdadesAnimales} title="Distribución de Animales" />
         </div>
       </div>
     </div>
